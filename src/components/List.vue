f75eca9dfd0d73e09e090fd1a963bd98eddbc0d7
Junya Kitahata <zyunn14641@gmail.com>
2020-06-23 22:16:00 +0900

    second versiton

---
+++ src/components/List.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class ="list">
+        <div class="listheader">
+            <p class="list-title">{{title}}</p>
+            <div class ="deletelist" @click="removelist">x</div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    props:{
+        title:{
+            type:String,
+            required: true
+        },
+        listIndex: {
+            type:Number,
+            required:true
+        }
+    },
+    methods:{
+        removeList: funtion(){
+            if(confirm('本当にこのリストを削除しますか？')){
+                this.$store.dispatch('removelist',{ listIndex:this.listIndex})
+            }
+        },
+    }
+}
+</script>
